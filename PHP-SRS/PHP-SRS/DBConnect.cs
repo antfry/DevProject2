da6a34fa0db31f9a05d5d887edfdd673fb579c69
Fortress502 <josh_rae_18@hotmail.com>
2016-05-26 23:12:46 +1000

    Sprint 2 Start
    Start of sprint 2

---
+++ PHP-SRS/PHP-SRS/DBConnect.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace PHP_SRS
+{
+    public class DBConnect
+    {
+        public MySqlConnection conn;
+        public string connString;
+
+        //Constructor
+        public DBConnect()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            string connString = "Server=110.22.83.243;Port=3306;Database=pharmacy;Uid=root;password=alpine12";
+            conn = new MySqlConnection(connString);
+        }
+
+
+        //Open Connection
+        public bool OpenConnection()
+        {
+            try
+            {
+                conn.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Unable to connect to server");
+                        break;
+                    case 1045:
+                        Console.WriteLine("Invalid user/pass");
+                        break;
+                    default:
+                        Console.WriteLine("Unknown Error");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        public bool CloseConnection()
+        {
+            try
+            {
+                conn.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ PHP-SRS/PHP-SRS/Main.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PHP_SRS
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+
+
+
+            StockTake st = new StockTake();
+            List<string> rows = st.GetNameRows("SELECT * FROM stocktable");
+
+            textBox1.Text = "";
+
+            int quantity;
+            string name;
+
+            if (rows.Count() == 0)
+            {
+                textBox1.Text = "There are no critical items.\n";
+            }
+
+            for (int i = 0; i < rows.Count(); i++)
+            {
+                name = st.GetName("" + (i + 1));
+                quantity = st.GetQuantity("" + (i + 1));
+
+                if (quantity < 50)
+                {
+                    textBox1.Text += name + " only has " + quantity + " items in stock!" + "\n";
+                }
+            }
+
+
+
+
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmStockItems m = new frmStockItems();
+            m.Show();
+            this.Hide();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            frmSales m = new frmSales();
+            m.Show();
+            this.Hide();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            frmReports m = new frmReports();
+            m.Show();
+            this.Hide();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

0124cb06bc9e4887147e985d8154670f88dde255
Fortress502 <josh_rae_18@hotmail.com>
2016-05-26 23:17:26 +1000

    Basic GUI Implementation
    The basic implementation for Sales and Items GUI

---
+++ PHP-SRS/PHP-SRS/frmAddNewItem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PHP_SRS
+{
+    public partial class frmAddNewItem : Form
+    {
+        public frmAddNewItem()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmStockItems m = new frmStockItems();
+            m.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string name;
+            string description;
+            string attribute;
+            string quantity;
+            double price;
+            name = textBox1.Text.ToString();
+            description = textBox2.Text.ToString();
+            attribute = "TEST";
+            quantity = textBox3.Text.ToString(); ;
+            price = Convert.ToDouble(textBox5.Text);
+            price = double.Parse(textBox5.Text);
+
+
+            //Method to Add Item into Database
+            AddItem ai = new AddItem();
+            ai.InsertIntoTable(name, description, attribute, quantity, price);
+
+
+            // MessageShow
+
+            MessageBox.Show("Item added to successfully.");
+
+        
+        }
+    }
+}

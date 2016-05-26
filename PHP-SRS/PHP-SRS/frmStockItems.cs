4c37c1b4e68b6a9608589c975edef9875452fdac
Fortress502 <josh_rae_18@hotmail.com>
2016-05-26 23:40:31 +1000

    Item & Forms change
    Updates to basic forms and refinement to Item

---
+++ PHP-SRS/PHP-SRS/frmStockItems.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PHP_SRS
+{
+    public partial class frmStockItems : Form
+    {
+        public frmStockItems()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            Main m = new Main();
+            m.Show();
+
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmAddNewItem m = new frmAddNewItem();
+            m.Show();
+            this.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            frmUpdateItem m = new frmUpdateItem();
+            m.Show();
+            this.Hide();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            frmViewAllStock m = new frmViewAllStock();
+            m.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ PHP-SRS/PHP-SRS/frmViewAllStock.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace PHP_SRS
+{
+    partial class frmViewAllStock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridStock = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridStock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridStock
+            // 
+            this.dataGridStock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridStock.Location = new System.Drawing.Point(36, 69);
+            this.dataGridStock.Name = "dataGridStock";
+            this.dataGridStock.Size = new System.Drawing.Size(662, 409);
+            this.dataGridStock.TabIndex = 1;
+            this.dataGridStock.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridSales_CellContentClick);
+            // 
+            // frmViewAllStock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 539);
+            this.Controls.Add(this.dataGridStock);
+            this.Controls.Add(this.button1);
+            this.Name = "frmViewAllStock";
+            this.Text = "frmViewAllStock";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridStock)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridStock;
+    }
+}
